9a388514ae18936f9c56adcf825af35b120aef44
Phil Pulsford <Phil Pulsford>
2016-01-24 20:08:44 +0000

    hsql changes

---
+++ Web/src/main/java/com/pulsford/test/web/Register.java
@@ -0,0 +1,53 @@
+package com.pulsford.test.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Register  extends HttpServlet {
+    Connection con;
+    @Override
+    public void init() throws ServletException {
+        try {
+        	Class.forName("org.hsqldb.jdbcDriver");
+       // 	Class.forName("org.hsqldb.jdbc.JDBCDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace(System.out);
+        }
+        try {
+        	// in memory
+            con=DriverManager.getConnection("jdbc:hsqldb:mem:aname","SA","");
+          //  con=DriverManager.getConnection("jdbc:hsqldb:testdb","SA","");
+            con.createStatement().executeUpdate("create table contacts (name varchar(45),email varchar(45),phone varchar(45))");
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+        String name=request.getParameter("name");
+        String email=request.getParameter("email");
+        String phone=request.getParameter("phone");
+        try {
+            PreparedStatement pst=con.prepareStatement("insert into contacts values(?,?,?)");
+            pst.clearParameters();
+            pst.setString(1, name);
+            pst.setString(2, email);
+            pst.setString(3, phone);
+            int i=pst.executeUpdate();
+            out.write(i+" records inserted, <a href='ViewRecords'>View Records</a>");
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+}
+
---
+++ Web/src/main/java/com/pulsford/test/web/ViewRecords.java
@@ -0,0 +1,55 @@
+package com.pulsford.test.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ViewRecords extends HttpServlet {
+    Connection con;
+    @Override
+    public void init() throws ServletException {
+        try {
+        	Class.forName("org.hsqldb.jdbcDriver");
+         //   Class.forName("org.hsqldb.jdbc.JDBCDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace(System.out);
+        }
+        try {
+        	// in memory
+            con=DriverManager.getConnection("jdbc:hsqldb:mem:aname","SA","");
+        //    con=DriverManager.getConnection("jdbc:hsqldb:testdb","SA","");
+
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+        try {
+            PreparedStatement pst=con.prepareStatement("select * from contacts");
+            pst.clearParameters();
+            ResultSet rs=pst.executeQuery();
+            while(rs.next()){
+                out.write("<br/>"+rs.getString(1));
+                out.write(", "+rs.getString(2));
+                out.write(", "+rs.getString(3));
+            }
+            out.write("<hr/><a href='index.html'>Home</a> ");
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+}
+
+
